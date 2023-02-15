7e7dae678177718976216b82c2a724f40fe3ac83
Sandeep Saini <sing1176@algonquinlive.com>
2023-02-15 13:45:42 -0500

    fetch data and saved to state

--- aa-project/src/App.tsx
+++ aa-project/src/App.tsx
@@ -1,8 +1,31 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+
 import Sidebar from './components/Sidebar';
 import ContentArea from './components/ContentArea';
 function App() {
+
+  const [photos, setPhotos] = useState([])
+
+
+const url = 'https://agencyanalytics-api.vercel.app/images.json'
  
+ const fetchPhotos = async () => {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			setPhotos(data);
+		} catch (error) {
+			console.error(error);
+		}
+ };
+
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+
 
   return (
     <div className="App">
